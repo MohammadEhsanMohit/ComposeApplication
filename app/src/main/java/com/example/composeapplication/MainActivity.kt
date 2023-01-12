2b1a35484325dd76de57af3c31601a9aed39098d
milad salimi <31917346+miladsalimiiii@users.noreply.github.com>
2023-01-12 23:43:22 +0330

    Add bottomBar , closed #6 (#7)
    Co-authored-by: miladsalimi <milad.salimi@snapp.cab>

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.example.composeapplication
 
+import BottomBar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.composeapplication.common.mainActivityConstraintSet
+import com.example.composeapplication.data.BottomNavigationItem
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 import com.example.composeapplication.view.*
 import com.example.composeapplication.view.recipe.RecipeCard
@@ -27,7 +36,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                RecipeList(recipes = recipeList)
+
+                MainScreenView()
+
+               // RecipeList(recipes = recipeList)
             }
         }
     }
@@ -39,21 +51,59 @@ fun MainActivityCustomView() {
         modifier = Modifier.wrapContentSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        BoxWithConstraints(Modifier.fillMaxSize(),
+        BoxWithConstraints(
+            Modifier.fillMaxSize(),
         ) {
             ConstraintLayout(mainActivityConstraintSet(5.dp, sampleRecipe.id)) {
                 ProfilePageNew()
-                RecipeCard(recipe = sampleRecipe,16.dp)
+                RecipeCard(recipe = sampleRecipe, 16.dp)
             }
         }
-
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeApplicationTheme {
         RecipeList(recipes = recipeList)
     }
+}
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavigationItem.Profile.screen_route) {
+        composable(BottomNavigationItem.Profile.screen_route) {
+            ProfilePage()
+        }
+        composable(BottomNavigationItem.ProfileList.screen_route) {
+            ProfilePageNew()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreenView() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomBar(navController = navController) }
+    ) { padding ->
+        NavHost(
+            navController,
+            startDestination = BottomNavigationItem.Profile.screen_route,
+            Modifier.padding(padding)
+        ) {
+            composable(BottomNavigationItem.Profile.screen_route) {
+                ProfilePage()
+            }
+            composable(BottomNavigationItem.ProfileList.screen_route) {
+                ProfilePageNew()
+            }
+
+        }
+
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/composeapplication/data/BottomNavigationItem.kt
@@ -0,0 +1,9 @@
+package com.example.composeapplication.data
+
+import com.example.composeapplication.R
+
+sealed class BottomNavigationItem(var title: String, var icon: Int, var screen_route: String) {
+
+    object Profile : BottomNavigationItem("Profile", R.drawable.icon_profile, "profile")
+    object ProfileList : BottomNavigationItem("ProfileList", R.drawable.icon_list, "profileList")
+}
