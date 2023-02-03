7889ec5bbc6fc6b7a354970ccc8303ae5a5adde2
Mohammad Ehsan Mohit <121758810+MohammadEhsanMohit@users.noreply.github.com>
2023-02-03 16:21:04 +0330

    issue #19. (#20)
    1. Added splash screen and navhost to activity.
    2. The splash has a loading screen. Once the loading is complete, navigate to the next screen.
    3. When the back button is pressed from the home screen, the splash screen will not appear.
    
    Co-authored-by: Ehsan Mohit <mohammadehsan.mohit@snapp.cab>

--- app/src/main/java/com/example/composeapplication/di/AppModule.kt
+++ app/src/main/java/com/example/composeapplication/di/AppModule.kt
@@ -2,10 +2,12 @@ package com.example.composeapplication.di
 
 import com.example.composeapplication.feature.note.presentation.add_edit_note.AddEditNoteViewModel
 import com.example.composeapplication.feature.note.presentation.notes.NotesViewModel
+import com.example.composeapplication.feature.splash.presentation.splash.SplashScreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel { NotesViewModel(get()) }
     viewModel { AddEditNoteViewModel(get(),get()) }
+    viewModel { SplashScreenViewModel() }
 }
